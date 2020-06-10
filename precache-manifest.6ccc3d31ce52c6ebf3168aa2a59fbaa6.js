c5cd76f23e309878d3cd7a56730bf967dee62b75
Roman Lysenko <skiptomy55@gmail.com>
2020-06-10 06:27:31 +0000

    Updates

---
+++ precache-manifest.6ccc3d31ce52c6ebf3168aa2a59fbaa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d1be986f1c3d464bbaa63cb1b045f4c",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "806dcd612cc24a9952a7",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.56eaeec7.chunk.css"
+  },
+  {
+    "revision": "5db9d4495fa1a428a13e",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.eb2a171e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.eb2a171e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "806dcd612cc24a9952a7",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.1fdf48ab.chunk.js"
+  },
+  {
+    "revision": "a069d904aa4872b402ba",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.a99992af.js"
+  }
+]);
